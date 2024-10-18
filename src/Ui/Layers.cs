56657542979aff76bf8ad2511d3300146ae0d8a4
MaximilianMcC <max@maximilian.co.nz>
2024-10-18 13:24:15 +1300

    Layers can be selected

--- src/Ui/Layers.cs
+++ src/Ui/Layers.cs
@@ -11,6 +11,18 @@ class Layers
 	{
 		// Check for if they press ctrl+shift+n to make a new layer
 		if (Raylib.IsKeyDown(KeyboardKey.LeftControl) && Raylib.IsKeyDown(KeyboardKey.LeftShift) && Raylib.IsKeyPressed(KeyboardKey.N)) Canvas.AddLayer();
+
+		// Check for if they click on a layer
+		List<Rectangle> layerPreviews = GetRectangles();
+		for (int i = 0; i < layerPreviews.Count; i++)
+		{
+			// Check if they're clicking on the layer
+			if (Raylib.IsMouseButtonPressed(MouseButton.Left))
+			{
+				if (Raylib.CheckCollisionPointRec(Raylib.GetMousePosition(), layerPreviews[i])) Canvas.SelectedLayerIndex = i;
+			}
+
+		}
 	}
 
 	public static void Render()
@@ -18,17 +30,15 @@ class Layers
 		// Draw the layers bar thingy
 		Raylib.DrawRectangleRec(new Rectangle(Raylib.GetScreenWidth() - layersWidth, 0, layersWidth, Raylib.GetScreenHeight()), Theme.BackgroundPrimary);
 
-		// Draw a smaller version of all the layers
-		// going downwards
-		float layerPreviewHeight = ((layersWidth - padding2) / Canvas.Width) * Canvas.Height;
-		Rectangle layerPreview = new Rectangle(Raylib.GetScreenWidth() - layersWidth + padding, padding, layersWidth - padding2, layerPreviewHeight);
-		for (int i = Canvas.Layers.Count - 1; i >= 0 ; i--)
+		// Draw layer previews on the side
+		List<Rectangle> layerPreviews = GetRectangles();
+		for (int i = 0; i < Canvas.Layers.Count; i++)
 		{
 			// Draw the transparent background
 			Raylib.DrawTexturePro(
 				Canvas.TransparentBackground,
 				new Rectangle(0, 0, Canvas.Width, -Canvas.Height),
-				layerPreview,
+				layerPreviews[i],
 				Vector2.Zero,
 				0f,
 				Color.White	
@@ -38,14 +48,35 @@ class Layers
 			Raylib.DrawTexturePro(
 				Canvas.Layers[i].Texture,
 				new Rectangle(0, 0, Canvas.Width, -Canvas.Height),
-				layerPreview,
+				layerPreviews[i],
 				Vector2.Zero,
 				0f,
 				Color.White	
 			);
+		}
+	}
+
+	private static List<Rectangle> GetRectangles()
+	{
+		// Store all of the rectangles
+		List<Rectangle> rectangles = new List<Rectangle>();
+
+		// Calculate the height of the layer, and also make
+		// a default rectangle thingy to copy off of (lazy)
+		float layerPreviewHeight = ((layersWidth - padding2) / Canvas.Width) * Canvas.Height;
+		Rectangle layerPreview = new Rectangle(Raylib.GetScreenWidth() - layersWidth + padding, padding, layersWidth - padding2, layerPreviewHeight);
+
+		// Loop over all layers and get the rectangle
+		for (int i = Canvas.Layers.Count - 1; i >= 0 ; i--)
+		{
+			// Save the current layer
+			rectangles.Add(layerPreview);
 
 			// Move downwards for the next layer
 			layerPreview.Y += layerPreview.Height + padding;
 		}
+
+		// Ka Pai
+		return rectangles;
 	}
 }
